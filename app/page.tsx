0114a659a61b7fb114fde1b234e3ad8cfd26a66c
ktleary <kevin@ktleary.com>
2023-12-09 15:07:44 -0500

    create initial home

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,63 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import React from 'react';
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+const lines = [
+  'I build innovative software',
+  'for companies and organizations',
+  'changing the world',
+];
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+interface LandingTextProps {
+  text: string;
 }
+
+const LandingText = ({text}: LandingTextProps) => (
+  <div className="text-center text-[5vmin] my-1 text-white text-opacity-87">
+    {text}
+  </div>
+);
+
+// let's create a call to action to enter the site -- it will go under the text
+// it has a weird white outline, lets remove it
+
+const ProjectsButton = () => (
+  <div className="flex justify-center mt-4">
+    <button
+      className="bg-cyan-700 hover:bg-cyan-200 w-28 h-12 text-white font-bold   border-0 outline-none focus:outline-none rounded-full text-lg"
+      data-testid="enter-button">
+      Projects
+    </button>
+  </div>
+);
+
+const CodeButton = () => (
+  <div className="flex justify-center mt-4">
+    <button
+      className="bg-white hover:bg-slate-200 w-28 text-black font-bold   border-0 outline-none focus:outline-none rounded-full text-lg ml-4"
+      data-testid="enter-button">
+      Code
+    </button>
+  </div>
+);
+
+const ButtonRow = props => (
+  <div className="flex justify-center mt-4">{props.children}</div>
+);
+
+const Home = () => (
+  <>
+    <div className="mt-18 text-white text-opacity-87 mt-40 tracking-tighter">
+      {lines.map(line => (
+        <LandingText key={line} text={line} />
+      ))}
+      <div
+        className="bg-orange-500 h-[2px] mx-auto my-4 w-[40vmin]"
+        data-testid="underline"></div>
+    </div>
+    <ButtonRow>
+      <ProjectsButton />
+      <CodeButton />
+    </ButtonRow>
+  </>
+);
+
+export default Home;
